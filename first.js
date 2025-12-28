0968ec498f3303e22c89227748c4d1695093d9b8
Santosh Kumar <web.santo2012@gmail.com>
2025-12-28 20:16:09 +0530

    require fs

--- first.js
+++ first.js
@@ -1,1 +1,7 @@
 console.log("Santosh coding is the best")
+
+const fs = require('fs');
+fs.writeFile("output.txt", "writing file", (err) => {
+  if(err) console.log("Error Occured");
+  else console.log('File Written Successfully');
+})
